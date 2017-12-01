2e1e65c55472ff061ba03ed30a576adb0e04c304
Adam Rhodes <adam@adamrhodes.com>
2017-11-30 22:36:25 -0500

    Got some very basic functionality added.

---
+++ app/src/main/java/edu/umd/scavengerhunt/scavengerhunt/Create.java
@@ -0,0 +1,60 @@
+package edu.umd.scavengerhunt.scavengerhunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+
+    public void gotoCreateTitle(View view)
+    {
+
+        Intent intent = new Intent(this, Create_Title.class);
+        startActivity(intent);
+
+    }
+
+
+
+
+
+
+    public void gotoExplore(View view)
+    {
+
+        Intent intent = new Intent(this, Explore.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoCreate(View view)
+    {
+
+        Intent intent = new Intent(this, Create.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoProfile(View view)
+    {
+
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoSearch(View view)
+    {
+
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/edu/umd/scavengerhunt/scavengerhunt/Explore.java
@@ -0,0 +1,49 @@
+package edu.umd.scavengerhunt.scavengerhunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Explore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+
+    public void gotoExplore(View view)
+    {
+
+        Intent intent = new Intent(this, Explore.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoCreate(View view)
+    {
+
+        Intent intent = new Intent(this, Create.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoProfile(View view)
+    {
+
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+
+    }
+
+    public void gotoSearch(View view)
+    {
+
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+
+    }
+
+
+}
